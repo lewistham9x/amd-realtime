125aadfb031b9b2a03f7c977049e9f5422ccc007
lewistham9x <garan2314@gmail.com>
2019-01-22 22:27:40 +0800

    Setup for development

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,15 +1,10 @@
-var baseURL = "https://akshatmittal.com/youtube-realtime/";
+var baseURL = 'localhost:8080/';
 if (typeof isEmbed == 'undefined') isEmbed = 0;
 if (typeof isCustomPage == 'undefined') isCustomPage = 0;
-if (window.location.hostname.indexOf("local.akshatmittal.com") < 0) {
-    if (window.location.protocol != "https:") window.location.replace("https:" + window.location.href.substring(window.location.protocol.length));
-    if (location.hostname.indexOf("akshatmittal.com") == -1) window.location.replace(baseURL + location.hash);
-    if ((window.top !== window.self) && isEmbed == 0) window.top.location.replace(window.self.location.href);
-    if ((window.top == window.self) && isEmbed) window.top.location.replace(baseURL + location.hash);
-}
-Array.prototype.shuffle = function () {
+Array.prototype.shuffle = function() {
     var i = this.length,
-        j, temp;
+        j,
+        temp;
     if (i == 0) return this;
     while (--i) {
         j = Math.floor(Math.random() * (i + 1));
@@ -18,5 +13,5 @@ Array.prototype.shuffle = function () {
         this[j] = temp;
     }
     return this;
-}
+};
 var YT = {};
